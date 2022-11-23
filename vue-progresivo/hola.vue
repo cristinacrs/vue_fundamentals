aa7a33f68884ef2f9ee8824c18e78c7b6ff4ae81
Cristina Cruz Cortés <cristinacruzcortes@gmail.com>
2022-11-22 20:03:06 -0600

    vue progresivo

---
+++ vue-progresivo/hola.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>Lorem ipsum dolor</p>
+    <div>{{text}}</div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                text: "Hola!"
+            }
+        }
+    };
+</script>
+
+<style>
+    p{
+        color: plum;
+        font-family: sans-serif;
+    }
+</style>
